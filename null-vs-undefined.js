fc6ac7e7e62c828c260cd614bc7586d6dec67898
R051299 <rashedrazzak77@gmail.com>
2023-05-06 14:57:02 +0600

    null undefiend

---
+++ null-vs-undefined.js
@@ -0,0 +1,14 @@
+ let name;
+ console.log(name);
+
+ function add(num1, num2) {
+     console.log(num1 + num2);
+ }
+ const result = add(13, 82);
+ console.log(result);
+
+ function add2(num1, num2) {
+     console.log(num1, num2);
+ }
+ const result2 = add2(12);
+ console.log(result2);
