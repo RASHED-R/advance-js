45ed0c056bc6b76b0f22de7f771535413f0f7d7c
R051299 <rashedrazzak77@gmail.com>
2023-05-06 10:19:56 +0600

    true false

---
+++ true-false.js
@@ -0,0 +1,39 @@
+//false value 
+
+// 0
+// ""
+// undefined 
+// null
+// Nan
+// false
+
+// truthy
+
+// "0"
+// " "
+// []
+// {}
+
+let name = "rashed";
+
+if (name) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
+
+let name1 = "";
+
+if (name1) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
+
+let name2;
+
+if (name2) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
